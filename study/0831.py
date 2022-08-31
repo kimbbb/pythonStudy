038a7a086514f2554d92adfd891db63b6fcef193
김민지 <wehouse201@naver.com>
2022-08-31 17:08:19 +0900

    0831

---
+++ study/0831.py
@@ -0,0 +1,75 @@
+# list = []
+# list.append(1)
+# list.append(2)
+# list.append(6)
+# list[-1]=2
+# print(list)
+
+# for i in 리스트(range()) :
+
+# a=int(input())
+# sum=1
+# for i in range(1, a+1):
+#     sum*=i
+# print(sum)
+
+# i=1
+# sum=0
+# while i<=10:
+#     sum+=i
+#     i=i+1
+# print(sum)
+
+# a=int(input())
+# for i in range(1, 10):
+#     print(a,'*',i,'=',a*i)
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if j+i==6:
+#             print(i,j)
+
+
+# a=int(input())
+# b=int(input())
+# def pizza(a):
+#     return a*a*3.14
+# def pizza(b):
+#     return b*b*3.14
+#
+# print(pizza(a)*2)
+# print(pizza(b))
+
+# rate=int(input())
+# hour=int(input())
+#
+# def weeklyPay(rate, hour):
+#     if hour<30:
+#         cash = rate * hour
+#         return cash
+#     elif hour>30:
+#         n = (rate * 30)+((rate*1.5) * (hour - 30))
+#         return n
+#
+# print(weeklyPay(rate, hour))
+
+
+# import turtle as t
+#
+# def tur(turt):
+#     for i in range(4):
+#         t.forward(90)
+#         t.left(90)
+#     return turt
+#
+# print(tur(tur))
+#
+# t.shape('turtle')
+
+temps=[28,31,33,35,27,26,25]
+for i in range(len(temps)):
+    print(temps[i], end=',')
+
+temps=[28,31,33,35,27,26,25]
+for element in temps:
+    print(element, end=',')
