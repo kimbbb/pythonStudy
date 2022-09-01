fdf215026374bbdc418244e0b282e7f53a74610b
김민지 <wehouse201@naver.com>
2022-09-01 15:54:47 +0900

    0901

--- study/0831.py
+++ study/0831.py
@@ -66,10 +66,60 @@
 #
 # t.shape('turtle')
 
-temps=[28,31,33,35,27,26,25]
-for i in range(len(temps)):
-    print(temps[i], end=',')
+# temps=[28,31,33,35,27,26,25]
+# for i in range(len(temps)):
+#     print(temps[i], end=',')
+#
+# temps=[28,31,33,35,27,26,25]
+# for element in temps:
+#     print(element, end=',')
 
-temps=[28,31,33,35,27,26,25]
-for element in temps:
-    print(element, end=',')
+# import random
+# n=random.randint(1,10)
+# print(n)
+#
+# import random as r
+# n=r.randint(1,10)
+# print(n)
+
+# import random as r
+# d=list(range(1,46))
+# r.shuffle(d)
+# print(d[:7])
+
+
+# import random as r
+#
+# g=list(range(1,46))
+# r.shuffle(g)
+# x=g[:6]
+# print('당첨번호 :',g[:6],'+', '[',g[6],']')
+#
+# good=0
+# count=0
+# for i in range(6):
+#     a = int(input('입력 : '))
+#
+#     if a==x[i]:
+#         count+=1
+#
+#     elif a==g[6]:
+#         good+=1
+#
+# print()
+# print('-'*40)
+#
+# if count==6:
+#     print('1등')
+# elif count==5 and good==1:
+#     print('2등')
+# elif count==5:
+#     print('3등')
+# elif count==4:
+#     print('4등')
+# elif count == 3:
+#     print('5등')
+# else:
+#     print('꽝~~ㅋ')
+
+
